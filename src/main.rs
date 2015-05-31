a89275b21ad9fed4ffda078fcb62a98741c2c55c
Jacob Clark <jacob.jh.clark@googlemail.com>
2015-05-31 23:56:54 +0100

    Adds basic threadding support [broken]

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ extern crate time;
 use hyper::Client;
 use hyper::header::Connection;
 use time::*;
+use std::thread;
+use std::sync::Arc;
 
 struct Request {
     elapsed_time: f64
@@ -22,27 +24,33 @@ impl Request{
         for r in requests {
             // https://github.com/rust-lang/rust/issues/24138 
             // let elapsed_time = (r.elapsed_time * 100.0).round() / 100.0;
-            total_request_times = total_request_times + elapsed_time;
+            //total_request_times = total_request_times + elapsed_time;
         }
 
-        total_request_times / requests.len()
+        //total_request_times / requests.len()
     }
 }
 
 fn main() {
-    let mut client = Client::new();
     let mut requests = Vec::new();
+    let arc_requests = Arc::new(requests);
 
     for _x in 0..100 {
-        let start = time::precise_time_s();
+        let handle = thread::spawn(move || {
+            let client = Client::new();
+            let child_requests = arc_requests.clone();
+            let start = time::precise_time_s();
         
-        let res = client.get("http://jacob.uk.com")
-            .header(Connection::close()) 
-            .send().unwrap();
+            let res = client.get("http://jacob.uk.com")
+                .header(Connection::close()) 
+                .send().unwrap();
 
-        let end = time::precise_time_s();
-        
-        requests.push(Request::new(end-start));
+            let end = time::precise_time_s();
+            
+            child_numbers.push(Request::new(end-start));
+        });
+
+        handle.join().unwrap()
     }
 
     Request::mean_time(requests);
