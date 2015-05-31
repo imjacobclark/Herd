eeda25f78b9dcfc7a548efab0e4a708aa44f260a
Jacob Clark <jacob.jh.clark@googlemail.com>
2015-05-31 21:59:22 +0100

    Checking in basic HTTP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+extern crate hyper;
+
+use hyper::Client;
+use hyper::header::Connection;
+
+struct Request {
+    status: String
+}
+
+impl Request{
+    fn new(status: &str) -> Request{
+        Request {
+            status: status.to_string(),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut client = Client::new();
+    let mut requests = Vec::new();
+
+    for _x in 0..100 {
+        let res = client.get("http://jacob.uk.com")
+            .header(Connection::close()) 
+            .send().unwrap();
+       
+        println!("Status: {}", res.status);
+
+        requests.push(Request::new("test"));
+    }
+
 }
