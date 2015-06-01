a19512d61ad851d034f7f56e62b349d4db9ab3b8
Jacob Clark <jacob.jh.clark@googlemail.com>
2015-06-01 08:55:58 +0100

    Adds basic threading

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use hyper::Client;
 use hyper::header::Connection;
 use time::*;
 use std::thread;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 
 struct Request {
     elapsed_time: f64
@@ -17,41 +17,27 @@ impl Request{
             elapsed_time: elapsed_time,
         }
     }
-
-    fn mean_time(requests: Vec<Request>) {
-        let mut total_request_times = 0;
-        
-        for r in requests {
-            // https://github.com/rust-lang/rust/issues/24138 
-            // let elapsed_time = (r.elapsed_time * 100.0).round() / 100.0;
-            //total_request_times = total_request_times + elapsed_time;
-        }
-
-        //total_request_times / requests.len()
-    }
 }
 
 fn main() {
-    let mut requests = Vec::new();
-    let arc_requests = Arc::new(requests);
+    let requests = Arc::new(Mutex::new(Vec::new()));
 
     for _x in 0..100 {
+        let mut client = Client::new();
+        let thread_items = requests.clone();
+
         let handle = thread::spawn(move || {
-            let client = Client::new();
-            let child_requests = arc_requests.clone();
             let start = time::precise_time_s();
         
-            let res = client.get("http://jacob.uk.com")
+            let _res = client.get("http://jacob.uk.com")
                 .header(Connection::close()) 
                 .send().unwrap();
 
             let end = time::precise_time_s();
             
-            child_numbers.push(Request::new(end-start));
+            thread_items.lock().unwrap().push((Request::new(end-start)));
         });
 
-        handle.join().unwrap()
+        handle.join().unwrap();
     }
-
-    Request::mean_time(requests);
 }
