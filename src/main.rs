4fafc43e6c084b6b1d28c8f28400f8fd2fa3ee4d
Jacob Clark <jacob.clark@bbc.co.uk>
2015-06-01 14:07:27 +0100

    Formats output

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ extern crate time;
 
 use hyper::Client;
 use hyper::header::Connection;
-use std::sync::{Arc, Mutex};
-use std::thread;
 use time::*;
+use std::thread;
+use std::sync::{Arc, Mutex};
 
 struct Request {
     elapsed_time: f64
@@ -23,16 +23,12 @@ fn main() {
     let requests = Arc::new(Mutex::new(Vec::new()));
     let mut threads = Vec::new();
 
-    for _x in 0..100 {
-        println!("Spawning thread: {}", _x);
-
+    for _x in 0..10 {
         let mut client = Client::new();
         let thread_items = requests.clone();
 
         let handle = thread::spawn(move || {
-            for _y in 0..100 {
-                println!("Firing requests: {}", _y);
-
+            for _y in 0..10 {
                 let start = time::precise_time_s();
             
                 let _res = client.get("http://jacob.uk.com")
@@ -41,6 +37,8 @@ fn main() {
 
                 let end = time::precise_time_s();
                 
+                println!("Thread {} | Request {} |  Duration: {}", _x, _y, end-start);
+
                 thread_items.lock().unwrap().push((Request::new(end-start)));
             }
         });
