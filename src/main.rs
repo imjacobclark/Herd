a2cbac966e968b38f15f92b981cf60dbaaf75751
Jacob Clark <jacob.clark@bbc.co.uk>
2015-06-01 12:49:33 +0100

    Thread joining

--- src/main.rs
+++ src/main.rs
@@ -21,23 +21,34 @@ impl Request{
 
 fn main() {
     let requests = Arc::new(Mutex::new(Vec::new()));
+    let threads = Arc::new(Mutex::new(Vec::new()));
 
     for _x in 0..100 {
+        println!("Spawning thread: {}", _x);
+
         let mut client = Client::new();
         let thread_items = requests.clone();
 
         let handle = thread::spawn(move || {
-            let start = time::precise_time_s();
-        
-            let _res = client.get("http://jacob.uk.com")
-                .header(Connection::close()) 
-                .send().unwrap();
+            for _y in 0..100 {
+                println!("Firing requests: {}", _y);
 
-            let end = time::precise_time_s();
+                let start = time::precise_time_s();
             
-            thread_items.lock().unwrap().push((Request::new(end-start)));
+                let _res = client.get("http://jacob.uk.com")
+                    .header(Connection::close()) 
+                    .send().unwrap();
+
+                let end = time::precise_time_s();
+                
+                thread_items.lock().unwrap().push((Request::new(end-start)));
+            }
         });
 
-        handle.join().unwrap();
+        threads.lock().unwrap().push((handle));
+    }
+
+    for t in threads.iter(){
+        println!("Hello World");
     }
 }
