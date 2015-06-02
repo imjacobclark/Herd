ea6d6a3ecf513ef8d619969e62a4a70485759092
Jacob Clark <jacob.jh.clark@googlemail.com>
2015-06-02 19:04:11 +0100

    Removing tuning

--- tuning/linux/tune.sh
+++
@@ -1,5 +0,0 @@
-echo 1000000000000 > /proc/sys/kernel/pid_max
-echo 1000000000000 > /proc/sys/kernel/threads-max
-echo '*       soft    nofile  1000000000000 \n*       hard    nofile  1000000000000' >>  /etc/security/limits.conf
-echo 1000000000000 > /proc/sys/fs/nr_open
-echo 1000000000000 > /proc/sys/fs/file-max
