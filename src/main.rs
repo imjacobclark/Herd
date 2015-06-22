272b87c6842ad6a59cd7d5dbd42950664c068ce5
Jacob Clark <jacob.jh.clark@googlemail.com>
2015-06-22 20:17:57 +0100

    Tabs->Spaces

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ mod request;
 extern crate nix;
 
 fn main() {
-	let threads = 10;
+    let threads = 10;
     let requests = 1;
 
-	for _x in 0..2{
-		nix::unistd::fork();
-	}
+    for _x in 0..2{
+        nix::unistd::fork();
+    }
 
-	herd::release(threads, requests);
+    herd::release(threads, requests);
 }
