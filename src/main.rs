556ae36ee57787be1bd57e0e5a812e5467a1e5ac
Jacob Clark <jacob.jh.clark@googlemail.com>
2015-06-23 09:10:59 +0100

    Improves process forking logic

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,19 @@ mod request;
 extern crate nix;
 
 fn main() {
-    let mut threads = 1;
-    let requests = 1;
+    let mut threads = 5000;
+    let requests = 100;
     let host = "http://jacob.uk.com";
 
-    let processes = threads / 100;
+    let process_threshold = 1000;
+    let processes = threads / process_threshold;
 
     for _x in 1..processes {
         let _pid = nix::unistd::fork();
     }
 
-    if threads >= 100 {
-    	herd::release(100, requests, host);
+    if threads >= process_threshold {
+    	herd::release(process_threshold, requests, host);
     }else{
     	herd::release(threads, requests, host);
     }
