94b444d51f4a7a60a00b8d13cb9b2ddda432d15f
Felix Kronlage <kronlage@bytemine.net>
2015-07-05 00:50:41 +0200

    switch to clap-rs for command line handling.
    The switch to clap-rs will allow much more complex comannd lines in the future.

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,50 @@
-use std::env;
-
 mod herd;
 mod request;
 
+#[macro_use]
+extern crate clap;
 extern crate nix;
 
+use clap::{Arg, App};
+
 fn main() {
-    let args: Vec<_> = env::args().collect();
 
-    if args.len() >= 4 {
-        let threads = args[1].parse::<i32>().unwrap();
-        let requests = args[2].parse::<i32>().unwrap();
-        let host = &args[3];
+    let matches = App::new("Herd")
+                        .version("0.0.1")
+                        .about("Does http loadtesting")
+                        .arg(Arg::with_name("threads")
+                                    .help("Sets the number of threads to spawn")
+                                    .long("threads")
+                                    .short("t")
+                                    .takes_value(true)
+                                    .required(true))
+                        .arg(Arg::with_name("requests")
+                                    .help("Sets the number of requests")
+                                    .long("requests")
+                                    .short("r")
+                                    .required(true)
+                                    .takes_value(true))
+                        .arg(Arg::with_name("host")
+                                    .help("Sets the host")
+                                    .required(true)
+                                    .index(1))
+                        .get_matches();
 
-    
-        let process_threshold = 1000;
-        let processes = threads / process_threshold;
+    let threads = value_t!(matches.value_of("threads"), i32).unwrap();
+    let requests = value_t!(matches.value_of("requests"), i32).unwrap();
+    let host = matches.value_of("host").unwrap();
 
-        for _x in 1..processes {
-            let _pid = nix::unistd::fork();
-        }
+    let process_threshold = 1000;
+    let processes = threads / process_threshold;
 
-        if threads >= process_threshold {
-        	herd::release(process_threshold, requests, host);
-        }else{
-        	herd::release(threads, requests, host);
-        }
-    }else{
-        println!("Error: didn't specify enough parameters. USAGE: Herd <THREADS(int)> <REQUESTS(int)> <HOST(str)>")
+    for _x in 1..processes {
+        let _pid = nix::unistd::fork();
     }
-    
+
+    if threads >= process_threshold {
+        herd::release(process_threshold, requests, host);
+    }else{
+        herd::release(threads, requests, host);
+    }
+
 }
