731f8a55e4946243a423b14b8bb462d8ce283cc7
Chris Bacon <chrisbacon2009@hotmail.com>
2023-02-23 15:56:40 +0000

    Update to modern Rust

--- src/herd.rs
+++ src/herd.rs
@@ -1,33 +1,56 @@
-use std::thread;
-use std::sync::{Arc, Mutex};
-use request;
+use std::sync::Arc;
+use tokio::sync::Mutex; // tokio async runtime
 
-pub fn release(threads: i64, requests: i64, host: &str) {
-    let request = Arc::new(Mutex::new(Vec::new()));
-    let mut child_threads = Vec::new();
+use crate::request::{self, HerdRequest};
 
-    for _x in 0..threads {
-        let request_clone = request.clone(); 
-        let host_clone = host.to_owned();
+async fn run_thread(
+    req: Arc<Mutex<Vec<HerdRequest>>>,
+    requests: i64,
+    virtual_user_id: i64,
+    host: String,
+) -> i64 {
+    let req_clone = req.clone();
 
-        let handle = thread::spawn(move || {
-            for _y in 0..requests {
-                println!("Spawning virtual user {}", _x);
-                let host_str = &host_clone;
-                request_clone.lock().unwrap().push((request::Request::new(request::Request::create_request(host_str))));
-            }
-        });
+    for _y in 0..requests {
+        println!("Spawning virtual user {}", virtual_user_id);
 
-        child_threads.push(handle);
+        req_clone.lock().await.push(request::HerdRequest::new(
+            request::HerdRequest::create_request(&host).await,
+        ));
     }
 
-    for t in child_threads.into_iter() {
-        let _child_threads = t.join();
+    return virtual_user_id;
+}
+
+pub async fn release(threads: i64, requests: i64, host: &String) {
+    let req = Arc::new(Mutex::new(Vec::new()));
+
+    let mut tasks = Vec::with_capacity(threads.try_into().unwrap());
+    for virtual_user_id in 0..threads {
+        let host_copy = host.clone();
+        let req_copy = req.clone();
+
+        tasks.push(tokio::spawn(run_thread(
+            req_copy,
+            requests,
+            virtual_user_id,
+            host_copy,
+        )));
     }
 
-    let lock_request = &mut request.lock().unwrap();
-    let mean = request::Request::calculate_mean(lock_request);
-    let requests = request::Request::total_requests_made(lock_request);
+    let mut child_threads: Vec<i64> = Vec::with_capacity(tasks.len());
+    for task in tasks {
+        child_threads.push(task.await.unwrap());
+    }
 
-    println!("I made a total of {} requests, the mean response time was: {} seconds.", requests, mean);
+    println!("{:?}", child_threads);
+
+    let lock_request = &mut req.lock().await;
+    let mean = request::HerdRequest::calculate_mean(lock_request);
+    let requests = request::HerdRequest::total_requests_made(lock_request);
+
+    println!(
+        "I made a total of {} requests, the mean response time was: {} seconds.",
+        requests, mean
+    );
 }
--- src/request.rs
+++ src/request.rs
@@ -1,45 +1,44 @@
-extern crate hyper;
-extern crate time;
+use hyper::{header, Body, Client, Method, Request};
+use std::time::Instant;
 
-use self::hyper::Client;
-use self::hyper::header::Connection;
-use self::time::*;
-
-pub struct Request {
-    pub elapsed_time: f64
+pub struct HerdRequest {
+    pub elapsed_time: f64,
 }
 
-impl Request{
-    pub fn new(elapsed_time: f64) -> Request {
-        Request {
+impl HerdRequest {
+    pub fn new(elapsed_time: f64) -> HerdRequest {
+        HerdRequest {
             elapsed_time: elapsed_time,
         }
     }
 
-    pub fn create_request(host: &str) -> f64 {
+    pub async fn create_request(host: &str) -> f64 {
         let client = Client::new();
-        let start = time::precise_time_s();
-            
-        let _res = client.get(host)
-            .header(Connection::close()) 
-            .send();
+        let req = Request::builder()
+            .header(header::CONNECTION, "Close")
+            .uri(host)
+            .method(Method::GET)
+            .body(Body::from(""))
+            .expect("Request builder");
 
-        if let Err(res) = _res {
-            println!("Err: {:?}", res);
+        let start = Instant::now();
+
+        let res = client.request(req).await;
+
+        if let Err(e) = res {
+            println!("Err: {:?}", e);
         } else {
-            _res.unwrap();
+            res.unwrap();
         }
 
-        let end = time::precise_time_s();
-
-        return end-start;
+        return start.elapsed().as_secs_f64();
     }
 
-    pub fn total_requests_made(requests: &mut Vec<Request>) -> i32 { 
+    pub fn total_requests_made(requests: &mut Vec<HerdRequest>) -> i32 {
         return requests.len() as i32;
     }
 
-    pub fn calculate_mean(requests: &mut Vec<Request>) -> f64 { 
+    pub fn calculate_mean(requests: &mut Vec<HerdRequest>) -> f64 {
         let mut total_duration = 0.0;
         let total_requests = requests.len() as f64;
 
@@ -47,32 +46,32 @@ impl Request{
             total_duration = total_duration + r.elapsed_time;
         }
 
-        return total_duration/total_requests as f64;
+        return total_duration / total_requests as f64;
     }
 }
 
 #[cfg(test)]
 mod test {
-    use super::Request;
-    
+    use super::HerdRequest;
+
     #[test]
     fn test_total_requests_made_returns_expected_integer() {
-        let mut requests = &mut Vec::new();
-        
+        let requests: &mut Vec<HerdRequest> = &mut Vec::new();
+
         for x in 0..5 {
-            requests.push(Request::new(0.56));
+            requests.push(HerdRequest::new(0.56));
         }
 
-        assert_eq!(5, Request::total_requests_made(requests));
+        assert_eq!(5, HerdRequest::total_requests_made(requests));
     }
 
     #[test]
     fn test_calculate_mean_returns_expected_integer() {
-        let mut requests = &mut Vec::new();
-        
-        requests.push(Request::new(0.56));
-        requests.push(Request::new(0.76));
+        let requests: &mut Vec<HerdRequest> = &mut Vec::new();
 
-        assert_eq!(0.66, Request::calculate_mean(requests));
+        requests.push(HerdRequest::new(0.56));
+        requests.push(HerdRequest::new(0.76));
+
+        assert_eq!(0.66, HerdRequest::calculate_mean(requests));
     }
 }
